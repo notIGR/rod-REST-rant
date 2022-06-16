764f246b0963002effae892685fdef3804e0cfb3
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-06-15 20:41:30 -0400

    added my own css on top of bootstrap

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+//modules and globals
 require("dotenv").config();
 const express = require("express");
 const app = express();
 
+//express settings
+app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
 
+//middleware
+
+//controllers and routes
 app.use("/places", require("./controllers/places"));
 
 app.get("/", (req, res) => {
@@ -15,4 +22,5 @@ app.get("*", (req, res) => {
   res.status(404).render("error404");
 });
 
+//listen for connections
 app.listen(process.env.PORT);
