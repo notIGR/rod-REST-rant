6defca9627b1209abaae9eeb162c0ac5777a6d96
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-06-18 12:53:08 -0400

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { render } = require("react-dom");
 const places = require("../models/places.js");
 
 // get/new
@@ -9,21 +10,33 @@ router.get("/new", (req, res) => {
 router.post("/", (req, res) => {
   if (!req.body.pic) {
     //default pic if none if provided
-    req.body.pic = "http://placekitten.com/400/400"
+    req.body.pic = "http://placekitten.com/400/400";
   }
   if (!req.body.city) {
-    req.body.city = "FlavorTown"
+    req.body.city = "FlavorTown";
   }
-  if(!req.body.state) {
-    req.body.state = "USA"
+  if (!req.body.state) {
+    req.body.state = "USA";
   }
-  places.push(req.body)
+  places.push(req.body);
   res.redirect("/places");
 });
 
 //get /places
 router.get("/", (req, res) => {
   res.render("places/index", { places });
+});
+
+//showpPage
+router.get("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/show", {place: places[id]});
+  }
 });
 
 module.exports = router;
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,10 +2,12 @@ const React = require("react");
 const Def = require("../default");
 
 const index = (data) => {
-const placesFormatted = data.places.map((place) => {
+  const placesFormatted = data.places.map((place, index) => {
     return (
       <div className="col-sm-6" key={place.name}>
-        <h2>{place.name}</h2>
+        <h2>
+          <a href={`/places/${index}`}>{place.name}</a>
+        </h2>
         {/* class name the text appers to side of image, clas name text appear above and below image */}
         <p className="text-center">{place.cuisines}</p>
         <img src={place.pic} alt={place.name} />
