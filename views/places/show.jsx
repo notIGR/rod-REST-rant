1676d8548ecf209295900497a1ef49267041f633
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-06-18 13:43:10 -0400

    added boostrap to show page for layout

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,11 +2,46 @@
 const React = require("react");
 const Def = require("../default");
 
-const showPage = () => {
+const showPage = (data) => {
   return (
     <Def>
       <main>
-        <h1>Show individual Information Here</h1>
+        <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+          <h1 className="display-4">{data.place.name}</h1>
+          <img src={data.place.pic} alt={data.place.name} />
+        </div>
+
+        <div className="container">
+          <div className="card-deck mb-3 text-center">
+            <div className="card mb-4 box-shadow">
+              <div className="card-header">
+                <h4 className="my-0 font-weight-normal">Rating</h4>
+              </div>
+              <div className="card-body">
+                <p>Not rated</p>
+              </div>
+            </div>
+            <div className="card mb-4 box-shadow">
+              <div className="card-header">
+                <h4 className="my-0 font-weight-normal">Description</h4>
+              </div>
+              <div className="card-body">
+                <p>
+                  Located in {data.place.city} , {data.place.state} and serving{" "}
+                  {data.place.cuisines}!
+                </p>
+              </div>
+            </div>
+            <div className="card mb-4 box-shadow">
+              <div className="card-header">
+                <h4 className="my-0 font-weight-normal">Comments</h4>
+              </div>
+              <div className="card-body">
+                <p>No comments yet!!!</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </Def>
   );
