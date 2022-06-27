f48938e34cd9fd6942da62980cb5794b8b8c466f
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-06-27 14:51:15 -0400

    re organized order of routes

--- controllers/places.js
+++ controllers/places.js
@@ -1,16 +1,44 @@
 const router = require("express").Router();
+const db = require("../models")
 const { render } = require("react-dom");
-const places = require("../models/places.js");
+const places = require("../models/places.js"); //wont need this require eventually?
 
-//edit
-router.get("/:id/edit", (req, res) => {
-  const id = Number(req.params.id);
+
+
+//get /places
+router.get("/", (req, res) => {
+  res.send("GET /places stub"); //seventh
+});
+
+router.post("/", (req, res) => {
+  if (!req.body.pic) {
+    //default pic if none if provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "FlavorTown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.send("POST /places stub"); //sixth
+});
+
+// get/new
+router.get("/new", (req, res) => {
+  res.render("places/new"); //fith
+});
+
+//showpPage
+router.get("/:id", (req, res) => {
+  let id = Number(req.params.id);
   if (isNaN(id)) {
-    res, render("error404");
+    res.render("error404");
   } else if (!places[id]) {
     res.render("error404");
   } else {
-    res.render("places/edit", { place: places[id], id });
+    res.send("GET /places/:id stub"); //fourth
   }
 });
 
@@ -36,7 +64,7 @@ router.put("/:id", (req, res) => {
     }
     // Save the new data into places[id]
     places[id] = req.body;
-    res.redirect(`/places/${id}`);
+    res.send("PUT .places/:id stub"); //third
   }
 });
 
@@ -49,44 +77,19 @@ router.delete("/:id", (req, res) => {
     res.render("error404");
   } else {
     places.splice(id, 1);
-    res.redirect("/places");
+    res.send("DELETE /places/:id stub"); //second
   }
 });
 
-// get/new
-router.get("/new", (req, res) => {
-  res.render("places/new");
-});
-
-router.post("/", (req, res) => {
-  if (!req.body.pic) {
-    //default pic if none if provided
-    req.body.pic = "http://placekitten.com/400/400";
-  }
-  if (!req.body.city) {
-    req.body.city = "FlavorTown";
-  }
-  if (!req.body.state) {
-    req.body.state = "USA";
-  }
-  places.push(req.body);
-  res.redirect("/places");
-});
-
-//get /places
-router.get("/", (req, res) => {
-  res.render("places/index", { places });
-});
-
-//showpPage
-router.get("/:id", (req, res) => {
-  let id = Number(req.params.id);
+//edit
+router.get("/:id/edit", (req, res) => {
+  const id = Number(req.params.id);
   if (isNaN(id)) {
-    res.render("error404");
+    res, render("error404");
   } else if (!places[id]) {
     res.render("error404");
   } else {
-    res.render("places/show", { place: places[id], id });
+    res.send("GET edit form stub"); //first thing to change from bottom
   }
 });
 
