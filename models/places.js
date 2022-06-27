08cd40ff2c34735a7ea12afec11beee018d1a3c7
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-06-27 14:20:02 -0400

    installed mongoose and made schema

--- models/places.js
+++ models/places.js
@@ -1,3 +1,16 @@
+const mongoose = require("mongoose");
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cusisines: { type: String, required: true },
+  city: { type: String, default: "Flavortown" },
+  state: { type: String, default: "USA" },
+  founded: Number,
+});
+
+module.exports = mongoose.model("Place", placeSchema)
+
 module.exports = [
   {
     name: "H-Thai-ML",
